3a4475561ab0c94837b15f9a912e01bbd4708f42
MounibNemmiche <MounibNemmiche@users.noreply.github.com>
2026-03-29 20:13:40 +0200

    Auto-verify email on registration for demo deployment

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -45,6 +45,7 @@ class CreateNewUser implements CreatesNewUsers
                 'name' => $input['name'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
+                'email_verified_at' => now(),
             ]);
 
             $user->assignRole('patient');
